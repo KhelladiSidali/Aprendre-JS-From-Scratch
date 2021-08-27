092517d2ae984b027cf7de6dc2e93069333bff08
Khelladi Sidali <khlld.sidali@gmail.com>
2021-08-28 00:07:16 +0100

    28/09/2021 /video 2 JS FROM SCRATCH / 3h59min (generateur de bulles

---
+++ generateur de bulle/index.js
@@ -0,0 +1,6 @@
+const bubble = document.createElement("span");
+// document.createElement("elemnt") tzid un element f html
+// console.log(bubble);
+
+document.body.appendChild(bubble);
+// haka 9olna l body 3andk Child (enfant) qui est span (bubble)
